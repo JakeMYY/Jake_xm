12a8297f23b5645478f80fc6a2232b49ede80463
Jake 小太阳 <myy@163.com>
2017-08-22 20:39:00 +0800

    项目插件

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <mt-button type="danger" size="large">愛してる</mt-button>
+</div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    },created(){
+      this.$ajax.get('getlunbo')
+      .then(res=>{
+        console.log(res);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+  }
+</script>
+<style lang="">
+  
+</style>
